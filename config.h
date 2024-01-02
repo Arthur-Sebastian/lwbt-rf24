3a0920688c3a9bd7ca52f459986dfeb8db706caa
Arthur-Sebastian <artursebastianmiller@gmail.com>
2024-01-02 18:04:34 +0100

    feat: implement multi-radio mode

--- config.h
+++ config.h
@@ -9,8 +9,29 @@
 #define CONFIG
 
 /**
+@brief Enables debug output on serial link.
+*/
+#define DEBUG
+
+/**
 @brief CPU clock speed.
 */
 #define F_CPU 16000000UL
 
+/**
+@brief Whether to use multiple radios.
+*/
+#define MULTI_RADIO
+
+/**
+@brief Channel listening duration in milliseconds.
+*/
+#define LISTEN_DURATION 20
+
+#define RADIO_A_SS  PC5
+#define RADIO_A_IRQ PC4
+#define RADIO_B_SS  PC3
+#define RADIO_B_IRQ PC2
+#define RADIO_C_SS  PC1
+#define RADIO_C_IRQ PC0
 #endif
--- main.c
+++ main.c
@@ -11,10 +11,15 @@
 #include "btle.h"
 
 
-#define BAUD 9600
+/* Effectively doubled by setting U2X0 bit in UCSR0A */
+#define BAUD 76800
 #define UBRR (F_CPU/16/BAUD-1)
 
 btle_t radio_a;
+#ifdef MULTI_RADIO
+btle_t radio_b;
+btle_t radio_c;
+#endif
 
 static const char toHex[] = {
 	'0', '1', '2', '3',
@@ -69,9 +74,10 @@ static void setup(void)
 	/* config UART */
 	UBRR0H = (uint8_t)(UBRR >> 8);
 	UBRR0L = (uint8_t) UBRR;
+	UCSR0A = (1 << U2X0);
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 	UCSR0C = (1 << USBS0) | (3 << UCSZ00);
-	uart_print("> START\n");
+	uart_print("> BOOT\n");
 }
 
 
@@ -79,16 +85,26 @@ ISR(PCINT1_vect)
 {
 	cli();
 
-	if((PINC & (1 << PC4)) == 0) {
+	if((PINC & (1 << RADIO_A_IRQ)) == 0) {
 		btle_load(&radio_a);
 		btle_decode(&radio_a);
 	}
+	#ifdef MULTI_RADIO
+	if((PINC & (1 << RADIO_B_IRQ)) == 0) {
+		btle_load(&radio_b);
+		btle_decode(&radio_b);
+	}
+	if((PINC & (1 << RADIO_C_IRQ)) == 0) {
+		btle_load(&radio_c);
+		btle_decode(&radio_c);
+	}
+	#endif
 
 	sei();
 }
 
 
-static void rf_diag(void)
+static void rf_diag(uint8_t radio_ss)
 {
 	uint8_t res = 0;
 
@@ -99,25 +115,25 @@ static void rf_diag(void)
 		RF_REG_SETRF,
 		RF_REG_SETRETR,
 		RF_REG_RXWP0,
-		RF_REG_RXADDR
+		RF_REG_RXADDR,
+		RF_REG_RFCH
 	};
 
-	uart_print("> DIAG\n");
+	uart_print("> DEBUG\n");
 	for (uint8_t i = 0; i < sizeof(regs); i++) {
-		spi_select(PC5);
+		spi_select(radio_ss);
 		spi_transfer(RF_CMD_RREG | regs[i]);
 		res = spi_transfer(RF_CMD_NOP);
-		spi_unselect(PC5);
+		spi_unselect(radio_ss);
 		uart_bin(res);
 	}
-	uart_print("> END DIAG\n");
+	uart_print(">\n");
 }
 
 
 void print_bytes(uint8_t* data, uint8_t length)
 {
 	for(uint8_t i = 0; i < length; i++) {
-		//uart_bin(*(data + i));
 		uart_hex(*(data + i));
 	}
 	uart_print("\n");
@@ -126,31 +142,63 @@ void print_bytes(uint8_t* data, uint8_t length)
 
 int main(void)
 {
-	radio_a.spi_ss = PC5;
+	setup();
 	uint8_t current = 0;
 
-	setup();
+	radio_a.spi_ss = RADIO_A_SS;
 	btle_init(&radio_a);
-	rf_diag();
+	#ifdef MULTI_RADIO
+	btle_set_channel(0, &radio_a);
+
+	radio_b.spi_ss = RADIO_B_SS;
+	btle_init(&radio_b);
+	btle_set_channel(1, &radio_b);
+
+	radio_c.spi_ss = RADIO_C_SS;
+	btle_init(&radio_c);
+	btle_set_channel(2, &radio_c);
+
+	btle_enable();
+	#endif
+
+
+	#ifdef DEBUG
+	rf_diag(RADIO_A_SS);
+	#ifdef MULTI_RADIO
+	rf_diag(RADIO_B_SS);
+	rf_diag(RADIO_C_SS);
+	#endif
+	#endif
 
 	while (1) {
+		#ifndef MULTI_RADIO
 		btle_enable();
-		_delay_ms(20);
+		_delay_ms(LISTEN_DURATION);
 		btle_disable();
 
 		current = (current < 2) ? current + 1 : 0;
 		btle_set_channel(current, &radio_a);
+		#endif
 
-		if (!btle_received(&radio_a)) {
-			continue;
+		cli();
+		if (btle_received(&radio_a)) {
+			print_bytes(radio_a.rx_buffer, radio_a.rx_len + 2);
+			print_bytes(radio_a.rx_buffer + radio_a.rx_len + 2, 3);
+			print_bytes((uint8_t *) &radio_a.rx_crc, 3);
 		}
-
-		uart_print("\nDUMP:\n");
-		print_bytes(radio_a.rx_buffer, radio_a.rx_len + 5);
-		uart_print("HEAD:\n");
-		print_bytes(radio_a.rx_buffer, 2);
-		uart_print("CRC:\n");
-		print_bytes((uint8_t *) &radio_a.rx_crc, 3);
+		#ifdef MULTI_RADIO
+		if (btle_received(&radio_b)) {
+			print_bytes(radio_b.rx_buffer, radio_b.rx_len + 2);
+			print_bytes(radio_b.rx_buffer + radio_b.rx_len + 2, 3);
+			print_bytes((uint8_t *) &radio_b.rx_crc, 3);
+		}
+		if (btle_received(&radio_c)) {
+			print_bytes(radio_c.rx_buffer, radio_c.rx_len + 2);
+			print_bytes(radio_c.rx_buffer + radio_c.rx_len + 2, 3);
+			print_bytes((uint8_t *) &radio_c.rx_crc, 3);
+		}
+		#endif
+		sei();
 	}
 
 	return 0;
